1267314c3dbfee20256188c1891433a27430b2d4
Hammad akhter <hammadakhter924@gmail.com>
2024-09-30 01:02:43 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,64 @@
+// var age = 18
+// age =prompt ("Enter your age");
+
+// if ( age != 10){
+//     alert ("You re not eligible")
+// }
+// else{
+//     alert("Invalied")
+// };
+
+
+//nested if statement//
+
+
+// var name = prompt ("Enter your name");
+
+// if ( name == "Hammad" ) {  
+//     alert("Name is correct");
+
+//     var age = prompt ("Enter your age");
+//     if ( age > 20) { 
+//         alert ("Age is correct")   
+//     }
+//      else {
+//         alert ("You are not eligible")
+//     }
+// } 
+// else {
+//     alert("Name is notcorrect");
+// }
+
+
+// var enter = prompt ("Enter your favrate number");
+
+// if (enter >= 80  ) {
+//     alert ("Your number is fav")
+// } 
+// else {
+//     alert("Your num is not fav")
+// }
+
+// var userinput = +prompt ("Enter any number");
+
+// if ( userinput% 3 == 0) {
+//     alert ( "your number is  correct")
+// }
+//  else {
+//     alert ( "your number is not correct") 
+// }
+
+
+var userinput = prompt ("Enter any number to check even or odd")
+
+if (userinput %2 == 0) {
+    alert ("your number is even");
+    console.log ( userinput + " is even")
+} else {
+    alert ("your number is odd");
+    console.log ( userinput + " is odd")
+
+
+}
+
+
